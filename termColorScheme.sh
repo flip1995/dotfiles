dd7cc0e9a70fea90e8bd6e8aad3d17f376c93830
flip1995 <uwdkn@student.kit.edu>
2016-07-23 19:10:54 +0200

    Adding gconf option to terminal color scheme script.

--- termColorScheme.sh
+++ termColorScheme.sh
@@ -75,4 +75,53 @@ if which "$DCONF" > /dev/null 2>&1; then
     fi
 fi
 
-echo "ERROR: Make sure that 'dconf' is installed on your system."
+# For older gnome versions without dconf.
+
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME="Base 16 Pop"
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG="base-16-pop"
+[[ -z "$GCONFTOOL" ]] && GCONFTOOL=gconftool
+[[ -z "$BASE_KEY" ]] && BASE_KEY=/apps/gnome-terminal/profiles
+
+PROFILE_KEY="$BASE_KEY/$PROFILE_SLUG"
+
+gset() {
+  local type="$1"; shift
+  local key="$1"; shift
+  local val="$1"; shift
+
+  "$GCONFTOOL" --set --type "$type" "$PROFILE_KEY/$key" -- "$val"
+}
+
+# Because gconftool doesn't have "append"
+glist_append() {
+  local type="$1"; shift
+  local key="$1"; shift
+  local val="$1"; shift
+
+  local entries="$(
+    {
+      "$GCONFTOOL" --get "$key" | tr -d '[]' | tr , "\n" | fgrep -v "$val"
+      echo "$val"
+    } | head -c-1 | tr "\n" ,
+  )"
+
+  "$GCONFTOOL" --set --type list --list-type $type "$key" "[$entries]"
+}
+
+# Append the Base16 profile to the profile list
+glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
+
+gset string visible_name "$PROFILE_NAME"
+gset string palette "#100030004000:#e7004c003c00:#1a0066002200:#f100c400f00:#34009800db00:#9b005900b600:#1a00bc009c00:#e000e000e000:#9500a500a600:#e7004c003c00:#2e00cc007100:#f100c400f00:#34009800db00:#9b005900b600:#1a00bc009c00:#ec00f000f100"
+gset string background_color "#100030004000"
+gset string foreground_color "#e000e000e000"
+gset string bold_color "#e000e000e000"
+gset bool   bold_color_same_as_fg "true"
+gset bool   use_theme_colors "false"
+gset bool   use_theme_background "false"
+
+unset PROFILE_NAME
+unset PROFILE_SLUG
+unset GCONFTOOL
+unset BASE_KEY
+exit 0
