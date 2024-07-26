2e0d461b8d49076a724f99338b0b201ac498a734
Philipp Krones <hello@philkrones.com>
2024-07-26 15:15:52 +0200

    lualine: show full path in inactive window. nvim

--- lua/settings.lua
+++ lua/settings.lua
@@ -57,6 +57,9 @@ require('lualine').setup({
     sections = {
         lualine_c = { { 'filename', path = 1 } }
     },
+    inactive_sections = {
+        lualine_c = { { 'filename', path = 1 } }
+    },
 })
 
 -- Usability
